94fea4eff24f0a826afe2da09a7c65432fe009ad
Ray1997 <694556566@qq.com>
2024-05-30 22:50:39 +0800

    - init my ai project

---
+++ src/main/java/net/ray1997/AiInkTimeCraftApplication.java
@@ -0,0 +1,13 @@
+package net.ray1997;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInkTimeCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInkTimeCraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ray1997/AiInkTimeCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ray1997;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInkTimeCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
