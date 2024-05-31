7a5d2aba230e95d93e5963b170335c9a9b45028e
yishang <chunlei.ycl@alibaba-inc.com>
2024-05-31 13:11:32 +0800

    to #1997 add chat html

--- src/main/java/net/ray1997/AiInkTimeCraftApplication.java
+++ src/main/java/net/ray1997/AiInkTimeCraftApplication.java
@@ -1,13 +1,28 @@
 package net.ray1997;
 
+import org.springframework.ai.autoconfigure.vectorstore.qdrant.QdrantVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.data.redis.RedisHealthContributorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.data.redis.RedisReactiveHealthContributorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.redis.LettuceMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, QdrantVectorStoreAutoConfiguration.class,
+        LettuceMetricsAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class,
+        RedisReactiveHealthContributorAutoConfiguration.class, RedisReactiveAutoConfiguration.class,
+        RedisHealthContributorAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class,
+        SecurityAutoConfiguration.class
+
+})
 public class AiInkTimeCraftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AiInkTimeCraftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AiInkTimeCraftApplication.class, args);
+    }
 
 }
